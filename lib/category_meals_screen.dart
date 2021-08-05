7176611922455ef4d50c9d96f3a328815ee03a27
Babanazar <babanazar@marti.tech>
2021-08-05 06:01:46 +0300

    dummy meals and display added

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-name';
+
   // final String categoryId;
   // final String categoryTitle;
   //
@@ -14,15 +16,19 @@ class CategoryMealsScreen extends StatelessWidget {
 
     final categoryTitle = routerArgs['title'];
     final categoryId = routerArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
-      body: Center(
-        child: Text(
-          'The Recipes For The Category!',
-        ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
